2cae11e90177b1b5d553e87d5340c687ce53c5fa
BoubakerMajdi <majdi.bbkr@gmail.com>
2025-01-23 20:07:41 +0100

    displaying the processed image

--- MobileApp/app/build.gradle.kts
+++ MobileApp/app/build.gradle.kts
@@ -7,6 +7,7 @@ android {
     namespace = "hilox.crop.documentcropping"
     compileSdk = 34
 
+
     defaultConfig {
         applicationId = "hilox.crop.documentcropping"
         minSdk = 24
@@ -15,6 +16,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    sourceSets {
+        getByName("main") {
+            assets.srcDirs("src/main/assets")
+        }
     }
 
     buildTypes {
@@ -33,14 +39,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    aaptOptions {
+        noCompress ("tflite")
+    }
+
 }
 
 dependencies {
 
+    implementation ("org.tensorflow:tensorflow-lite:2.5.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("com.google.ai.edge.litert:litert-support-api:1.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
